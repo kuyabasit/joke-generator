c6d5539ed2813ad11f7125fafee191ace4dc29e0
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-06-23 21:17:02 +0800

    last commit

--- app.js
+++ app.js
@@ -10,5 +10,22 @@ function getJokes(e) {
     true
   );
 
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      let output = "";
+
+      if (response.type === "success") {
+        response.value.forEach(function (joke) {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += "<li>Something went wrong</li>";
+      }
+      document.querySelector(".jokes").innerHTML = output;
+    }
+  };
+  xhr.send();
   e.preventDefault();
 }
